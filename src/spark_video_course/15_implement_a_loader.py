4c0185d966bbfcc967c5fee12b34a26e4a5542df
atrox3d <atrox3d@gmail.com>
2025-08-16 09:16:46 +0000

    15 implement loader

---
+++ src/spark_video_course/15_implement_a_loader.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession, Window, DataFrame
+from pyspark.sql.functions import col, year, row_number
+from pyspark.sql.types import StringType, DateType, DoubleType
+from pathlib import Path
+
+
+PROJECT_PATH = Path(__file__).parent.parent.parent
+DATA_PATH = PROJECT_PATH / 'data'
+assert DATA_PATH.exists()
+
+
+
+spark :SparkSession = SparkSession.builder    \
+        .appName("load-csv") \
+        .master("local[*]")     \
+        .getOrCreate()
+
+print(f"Spark version: {spark.version}")
+
+
+def load_stock_data(symbol:str, datapath:str|Path=DATA_PATH) -> DataFrame:
+    csv_path = f'{datapath!s}/{symbol}.csv'
+    print(f'loading {csv_path}...')
+
+    # Read the data, inferring the schema for correct data types.
+    df = spark.read.csv(
+        csv_path,
+        header=True, 
+        # inferSchema=True
+    )
+    
+    # specify schema instead of inferring it
+    # and rename columns
+    return df.select(
+        df['Date'].cast(DateType()).alias('date'),
+        df['Open'].cast(DoubleType()).alias('open'),
+        df['Close'].cast(DoubleType()).alias('close'),
+        df['High'].cast(DoubleType()).alias('high'),
+        df['Low'].cast(DoubleType()).alias('low'),
+    )
+
+aapl = load_stock_data('AAPL')
+aapl.printSchema()
+aapl.show(5)
+
+spark.stop()

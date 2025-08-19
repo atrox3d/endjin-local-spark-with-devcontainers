8e1e28aac7faa07f29ace8f2bd45eef752b3cb26
atrox3d <atrox3d@gmail.com>
2025-08-19 05:15:50 +0000

    add basic test structure

---
+++ tests/pyspark_structured_streaming_live/purchase_analytics_table_test.py
@@ -0,0 +1,24 @@
+import unittest
+from pyspark.sql import SparkSession, DataFrame
+
+
+class PurchaseAnalyticsTableTest(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        """Set up SparkSession for all tests."""
+        cls.spark = (
+            SparkSession.builder.appName("hcp-test")
+            .master("local[*]")
+            .getOrCreate()
+        )
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """Stop SparkSession after all tests."""
+        cls.spark.stop()
+
+    
+    def test_filter_purchases(self):
+        pass

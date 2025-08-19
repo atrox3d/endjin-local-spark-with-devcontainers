fff88dfbe61d6f3823ef6ec92e23f9d65beaa020
atrox3d <atrox3d@gmail.com>
2025-08-19 07:38:59 +0000

    add some spark session info

--- tests/pyspark_structured_streaming_live/purchase_analytics_table_test.py
+++ tests/pyspark_structured_streaming_live/purchase_analytics_table_test.py
@@ -11,18 +11,30 @@ class PurchaseAnalyticsTableTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up SparkSession for all tests."""
+        print("starting spark session...")
         cls.spark = (
-            SparkSession.builder.appName("hcp-test")
+            SparkSession.builder.appName("purchase-analytics-test")
             .master("local[*]")
+            .config("spark.driver.bindAddress", "127.0.0.1")
             .getOrCreate()
         )
+        # You can access the core properties directly from the SparkSession and its SparkContext.
+        sc = cls.spark.sparkContext
+        print("\n--- Spark Session Info ---")
+        print(f"  Version: {cls.spark.version}")
+        print(f"  Master: {sc.master}")
+        print(f"  App Name: {sc.appName}")
+        # The uiWebUrl property provides the link to the Spark UI.
+        print(f"  Web UI: {sc.uiWebUrl}")
+        print("--------------------------\n")
 
 
     @classmethod
     def tearDownClass(cls):
         """Stop SparkSession after all tests."""
+        print("stopping spark session...")
         cls.spark.stop()
-
+        print("spark session stopped")
     
     def test_filter_purchases(self):
         pass

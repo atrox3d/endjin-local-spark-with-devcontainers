fea1dc1de742f272e2ad1eaa1bbe1c95800d5f40
atrox3d <atrox3d@gmail.com>
2025-08-20 11:23:32 +0000

    add empty client method

--- src/pyspark_structured_streaming_live/purchase_analytics.py
+++ src/pyspark_structured_streaming_live/purchase_analytics.py
@@ -5,6 +5,11 @@ from pyspark.sql import DataFrame
 class PurchaseAnalytics:
     
     @staticmethod
+    def product_aggregations(df: DataFrame) -> DataFrame:
+        return df
+    
+    
+    @staticmethod
     def filter_purchases(df: DataFrame, upper_bound:float=10.0, lower_bound:float=1.0) -> DataFrame:
         total_spend = F.col('quantity') * F.col('pricePerUnit')
         filter_cond = (total_spend <= upper_bound) & (total_spend >= lower_bound)
